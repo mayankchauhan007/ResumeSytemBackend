298f1f927f5161a93a925087c9b79f1a6d5698f4
mayankchauhan007 <mayankchauhan20837@gmail.com>
2023-05-06 14:56:18 +0530

    added helper package, deleted NoOpPasswordEncoder file

---
+++ src/main/java/com/exam/helper/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.exam.helper;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException()
+    {
+        super("User with this username is not found in the DB ");
+    }
+    public UserNotFoundException(String msg) {
+        super(msg);
+    }
+}
